87e8510beeb57949d11d7d0f002fc65a9e6678c3
Emmanuel <emmanuel.lucio.urbina@outlook.com>
2023-03-19 02:53:23 -0500

    loader

--- app/views/product/products.php
+++ app/views/product/products.php
@@ -47,8 +47,77 @@
 
         .grilla {
             display: grid;
+
             grid-template-columns: repeat(3, 1fr);
             gap: 5px;
+        }
+
+        /** */
+        .lds-ellipsis {
+            display: inline-block;
+            position: relative;
+            width: 80px;
+            height: 80px;
+        }
+
+        .lds-ellipsis div {
+            position: absolute;
+            top: 33px;
+            width: 13px;
+            height: 13px;
+            border-radius: 50%;
+            background: #483cd7;
+            animation-timing-function: cubic-bezier(0, 1, 1, 0);
+        }
+
+        .lds-ellipsis div:nth-child(1) {
+            left: 8px;
+            animation: lds-ellipsis1 0.6s infinite;
+        }
+
+        .lds-ellipsis div:nth-child(2) {
+            left: 8px;
+            animation: lds-ellipsis2 0.6s infinite;
+        }
+
+        .lds-ellipsis div:nth-child(3) {
+            left: 32px;
+            animation: lds-ellipsis2 0.6s infinite;
+        }
+
+        .lds-ellipsis div:nth-child(4) {
+            left: 56px;
+            animation: lds-ellipsis3 0.6s infinite;
+        }
+
+        @keyframes lds-ellipsis1 {
+            0% {
+                transform: scale(0);
+            }
+
+            100% {
+                transform: scale(1);
+            }
+        }
+
+        @keyframes lds-ellipsis3 {
+            0% {
+                transform: scale(1);
+            }
+
+            100% {
+                transform: scale(0);
+            }
+        }
+
+        @keyframes lds-ellipsis2 {
+            0% {
+                transform: translate(0, 0);
+            }
+
+            100% {
+                transform: translate(24px, 0);
+            }
         }
     </style>
 </head>
@@ -158,38 +227,45 @@
 
         get_all_category();
 
-        function get_all_category() {
+        async function get_all_category() {
             const grilla = document.querySelector(".grilla");
-            grilla.innerHTML="";
-            axios.get("index.php?controller=product&action=get_all_products")
-                .then(response => {
-                    $data = response.data;
+            grilla.innerHTML = `<div></div><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>`;
 
-                    $data.forEach(element => {
-                        const c = document.createElement("card-product");
-                        c.setAttribute("data-name", `${element.nombre} - $ ${element.precio_unitario}`);
-                        c.setAttribute("data-image", "data:image/png;base64," + element.imagen)
-                        grilla.appendChild(c);
-                    });
-                })
-                .catch(error => console.error(error))
+            try {
+                const res = await axios.get("index.php?controller=product&action=get_all_products");
+                grilla.innerHTML = "";
+                res.data.forEach(element => {
+                    const c = document.createElement("card-product");
+                    c.setAttribute("data-name", `${element.nombre} - $ ${element.precio_unitario}`);
+                    c.setAttribute("data-image", "data:image/png;base64," + element.imagen);
+                    grilla.appendChild(c);
+                });
+            } catch (error) {
+                console.log(error);
+            }
+
+
         }
 
-        function get_by_category(category) {
+        async function get_by_category(category) {
             const grilla = document.querySelector(".grilla");
-            grilla.innerHTML = "";
-            axios.get("index.php?controller=product&action=get_all_products&categoria=" + category)
-                .then(response => {
-                    $data = response.data;
+            grilla.innerHTML = `<div></div><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>`;
 
-                    $data.forEach(element => {
-                        const c = document.createElement("card-product");
-                        c.setAttribute("data-name", `${element.nombre} - $ ${element.precio_unitario}`);
-                        c.setAttribute("data-image", "data:image/png;base64," + element.imagen)
-                        grilla.appendChild(c);
-                    });
-                })
-                .catch(error => console.error(error))
+            try {
+                const res = await axios.get("index.php?controller=product&action=get_all_products&categoria=" + category);
+
+                grilla.innerHTML = "";
+                res.data.forEach(element => {
+                    const c = document.createElement("card-product");
+                    c.setAttribute("data-name", `${element.nombre} - $ ${element.precio_unitario}`);
+                    c.setAttribute("data-image", "data:image/png;base64," + element.imagen);
+                    grilla.appendChild(c);
+                });
+            } catch (error) {
+                console.log(error);
+            }
+
+
         }
     </script>
 
